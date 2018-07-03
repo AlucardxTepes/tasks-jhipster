3bfef15af385b3dfd4606c873714874e3c58d78a
Alucard <alucard.557@gmail.com>
2018-07-02 22:52:17 -0400

    Init project, add Task entity

--- src/main/java/com/alucard/config/CacheConfiguration.java
+++ src/main/java/com/alucard/config/CacheConfiguration.java
@@ -38,6 +38,7 @@ public class CacheConfiguration {
             cm.createCache(com.alucard.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(com.alucard.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(com.alucard.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.alucard.domain.Task.class.getName(), jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
